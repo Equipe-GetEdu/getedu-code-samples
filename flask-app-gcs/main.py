128d156d523d85715f74d35050952a6042b35f3c
Ian Zdanowsky <ianzpimentel@gmail.com>
2023-05-22 09:02:03 -0300

    modified bucket name to none

--- flask-app-gcs/main.py
+++ flask-app-gcs/main.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request
 from google.cloud import storage
 import requests
 
-BUCKET_NAME = os.environ.get("BUCKET_NAME")
+BUCKET_NAME = ""
 
 
 # Inicia o objeto do framework Flask para construção da página Web
@@ -19,12 +19,6 @@ blog_post = {
     'content': 'Vamos utilizar o Google Cloud Storage?'
 }
 
-# Função para simular teste de carga no servidor
-def simulate_high_resource_utilization():
-    random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=100000))
-    random_string.upper()
-    time.sleep(0.2)
-
 
 ######################################################
 
@@ -35,12 +29,6 @@ def home():
     return render_template('upload.html', post=blog_post)
 
 
-# Endpoint do teste de carga
-@app.route('/load')
-def saturation():
-    simulate_high_resource_utilization()
-    return render_template('blog.html', post=blog_post)
-
 @app.route('/upload', methods=['POST'])
 def upload():
     image = request.files['image']
@@ -49,7 +37,7 @@ def upload():
     destination_blob_name = str(time.time())
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_file(image, content_type='image/jpeg')
-    return render_template('blog.html', post=blog_post)
+    return render_template('upload.html', post=blog_post)
 
 
 # Inicializar o app
